077dfbd994ce7e25e1738e21701c959866e14790
Ryan Patrick Kyle <ryan@plot.ly>
2018-11-28 13:55:03 -0500

    Updated package to include new docs

--- R/detach_all.R
+++ R/detach_all.R
@@ -1,4 +1,21 @@
-detach_all <- function(verbose=TRUE) {
+#' Detach All Packages 
+#'
+#' This function will attempt to detach all packages loaded by the user during the current R session. 
+#'
+#' @usage detach_all(verbose=TRUE, unload=TRUE) 
+#' 
+#' @param verbose Should a message be printed to the console naming all the detached packages? \code{TRUE} or \code{FALSE}.
+#' 
+#' @param unload Should the packages be unloaded from the namespace? \code{TRUE} or \code{FALSE}. 
+#'
+#' @details It's probably smarter to restart R, but being able to detach and unload all packages can be handy at times. 
+#'
+#' @author Ryan Kyle, \email{ryan.kyle@mail.mcgill.ca}
+#' @keywords utilities
+#'
+#' @export
+#'
+detach_all <- function(verbose=TRUE, unload=TRUE) {
   pkgList <- names(sessionInfo()$otherPkgs)
   if(!is.null(pkgList)) {
     if(verbose) {
@@ -10,6 +27,6 @@ detach_all <- function(verbose=TRUE) {
             sep=""),
       detach,
       character.only=TRUE,
-      unload=TRUE))    
+      unload=unload))    
   } else(stop("No loaded packages to detach!"))
 }
